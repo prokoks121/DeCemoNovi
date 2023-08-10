800950fd71f59e81da9ebed235c1e8ea2cc0f081
Mihailo Aleksić <mihailo.aleksic@partner.digitalchargingsolutions.com>
2023-08-10 14:28:11 +0200

    Fix images

--- app/src/main/java/com/example/decemo/model/VrstaLokala.kt
+++
@@ -1,8 +0,0 @@
-//package com.example.decemo.model
-//
-//data class VrstaLokala(
-//        val slika: Int,
-//        val vrsta: String,
-//        var status: Boolean,
-//        val id: Int
-//)
